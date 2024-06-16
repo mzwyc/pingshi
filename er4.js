4e61315bec6b9e1b2b400cbc05e44f4ce47daf3f
mzwyc <163634598+mzwyc@users.noreply.github.com>
2024-06-16 17:21:53 +0800

    Update er4.js

--- er4.js
+++ er4.js
@@ -4,8 +4,7 @@
 // @version      2024-06-15
 // @description  try to take over the world!
 // @author       You
-// @match        https://class.luogu.com.cn/learn/yugu24jcf12/
-// @match        https://*.luogu.com.cn/*
+
 // @match        https://*.*.*/*
 // @match        https://*.*/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=luogu.com.cn
